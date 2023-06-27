66020abd60fde2b6bae3cf4b1bce4557f9cf2aa4
santiago <bassilsantiago@gmail.com>
2023-06-27 00:27:35 -0500

    Todo: implement items tracking logic

---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,10 @@
+import NavBar from "./NavBar"
+
+export default function Cart() {
+    return (
+        <div>
+            <NavBar/>
+            <h1>Your cart's empty</h1>
+        </div>
+    )
+}
