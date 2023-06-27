dc84a72ba675381b40d93de450a9679a89a76cc1
santiago <bassilsantiago@gmail.com>
2023-06-27 00:26:56 -0500

    Todo: Add more routes for specific items

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import Shop from "./Pages/Shop";
+import Cart from "./Pages/Cart";
 
 const Router = () => {
   return (
@@ -10,6 +11,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
