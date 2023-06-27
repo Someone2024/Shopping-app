266f697933c099a669b848a05e3c050d21ed4ebd
santiago <bassilsantiago@gmail.com>
2023-06-27 00:28:15 -0500

    Feat: main home screen button added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,11 @@
 import NavBar from "./NavBar"
+import { Link } from "react-router-dom";
 
 export default function Home() {
     return (
         <div>
             <NavBar/>
-            <h1>Home</h1>
+            <button id="shopBtn"><Link to="/shop">Go Shopping</Link></button>
         </div>
     )
 }
