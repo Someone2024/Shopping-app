fbae261e3ea928d83953e1ca58b111be5205a7ec
santiago <bassilsantiago@gmail.com>
2023-06-27 00:27:53 -0500

    Feat: new routes added

--- src/Pages/NavBar.jsx
+++ src/Pages/NavBar.jsx
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
     return (
         <ul>
+          <h1><Link to="/">NSFW SHOP</Link></h1>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/Shop">Shop</Link></li>
+          <li><Link to="/cart">cart icon</Link></li>
         </ul>
     )
 }
