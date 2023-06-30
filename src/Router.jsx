bdf6699848d42733efa1edfd97fd844304e14a33
santiago <bassilsantiago@gmail.com>
2023-06-29 19:17:39 -0500

    import shop related components

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import Nav from "./Components/NavBar";
+import Home from "./Components/Home";
+import Shop from "./Components/Shop";
+import {Cart} from "./Components/Cart";
+import CheckOut from "./Components/CheckOut";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./Pages/Home";
-import Shop from "./Pages/Shop";
-import Cart from "./Pages/Cart";
-
-const Router = () => {
+export default function Router() {
   return (
     <BrowserRouter>
+    <Nav/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Shop" element={<Shop />} />
+        <Route path="/shop" element={<Shop />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/item" element={<CheckOut/>} />
       </Routes>
     </BrowserRouter>
   );
-};
-
-export default Router;
+}
